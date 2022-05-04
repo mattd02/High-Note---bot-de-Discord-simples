a33767641001cfffd1e840d4a5b7defd17e0942d
Matheus Dutra <matheustd2002@gmail.com>
2022-05-04 12:21:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run("ODk3MTY2MDkwNTkxOTUyOTQ3.YWRtCA.aaUYja7_ANy1ma1MZAXPe2f5Sss")
+client.run("Application ID do Discord")
